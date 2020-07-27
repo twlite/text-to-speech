2bb0fecf4891b1c96e47de733b5384a52fd0f55e
Snowflake <devsnowflake107@gmail.com>
2020-07-27 15:20:11 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function toSpeech() {
         const utterance = new SpeechSynthesisUtterance();
         utterance.text = text;
         utterance.pitch = 1;
-        utterance.volume = 1000;
         window.speechSynthesis.speak(utterance);
     } catch(e) {
         alert(e.message);
